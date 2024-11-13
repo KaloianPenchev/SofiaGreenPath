3f6fe68e9c3265f5c72612152158a2499cac2d9f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-11-13 17:55:33 +0200

    styling

--- frontend/sofia-greenpath/src/components/home-page-components/search-bar/RouteOptions.jsx
+++ frontend/sofia-greenpath/src/components/home-page-components/search-bar/RouteOptions.jsx
@@ -16,7 +16,7 @@ const RouteOptions = ({
     <section className="route-options-container">
       <button className="route-options-button" type="button">
         <figure className="button-content">
-          <FontAwesomeIcon style={{ fontSize: '0.8vw' }} icon={faSliders} />
+          <FontAwesomeIcon style={{ fontSize: '0.8rem' }} icon={faSliders} />
           <figcaption>Ниво на чистота</figcaption>
         </figure>
         <label className="switch">
@@ -29,10 +29,11 @@ const RouteOptions = ({
           <span className="slider"></span>
         </label>
         <FontAwesomeIcon
-          style={{ fontSize: '0.95vw'}}
+          style={{ fontSize: '0.95rem'}}
           icon={faCircleInfo}
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
+          className="icon-info-1"
         />
         {isHovered && (
           <span className="tooltip">
